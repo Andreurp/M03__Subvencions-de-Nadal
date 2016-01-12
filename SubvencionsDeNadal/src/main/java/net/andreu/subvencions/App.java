cfdccb62b275dab2458b5994c6bdf2af232f7842
Andreu <Andreu@192.168.15.134>
2016-01-12 16:21:02 +0100

    commit inicial

---
+++ SubvencionsDeNadal/src/main/java/net/andreu/subvencions/App.java
@@ -0,0 +1,13 @@
+package net.andreu.subvencions;
+
+/**
+ * Subvencions de Nadal
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
